08f19434c409f19a2736d1bbb6bada9b1f75907f
bellangerq <bellanger.wall@gmail.com>
2015-08-20 22:55:57 +0200

    Add HTML errors

---
+++ js/validation.js
@@ -0,0 +1,32 @@
+function displayTest() {
+  document.getElementById("email").style.color = "";
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+  /*var value = document.forms[""]["email"].value;
+
+  if (value !== /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/) {
+
+      //mettre le form action en none
+
+      //met la bordure en rogue
+    getElementById("email").style.border = "red";
+
+      //met en display : inline l'erreur
+    ("#format").show("slow");
+
+  } else {
+      //rien ?
+  }
+});*/
